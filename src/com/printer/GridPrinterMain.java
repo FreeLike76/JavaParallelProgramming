248c9246dbcd0a834cdca41b2f5486c523a0a611
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2022-02-10 13:57:10 +0200

    Task Complete: Wait notify

--- src/com/printer/GridPrinterMain.java
+++ src/com/printer/GridPrinterMain.java
@@ -1,7 +1,39 @@
 package com.printer;
 
 public class GridPrinterMain {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        LinePrinter printer = new LinePrinter();
+
+        Thread threadHorizontal = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        printer.printHorizontal();
+                    }
+                } catch (InterruptedException e) {
+
+                }
+
+            }
+        });
+
+        Thread threadVertical = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        printer.printVertical();
+                    }
+                } catch (InterruptedException e) {
+
+                }
+            }
+        });
+
+        threadHorizontal.start();
+        threadVertical.start();
     }
+
 }
---
+++ src/com/printer/LinePrinter.java
@@ -0,0 +1,34 @@
+package com.printer;
+
+public class LinePrinter {
+    public boolean lastHorizontal = true;
+    public final int MAX_PER_LINE = 100;
+    public int symbolsInLine = 0;
+
+    public void checkNewLine(){
+        if (symbolsInLine == MAX_PER_LINE){
+            System.out.print("\n");
+            symbolsInLine = 0;
+        }
+    }
+    public synchronized void printHorizontal() throws InterruptedException {
+        while(lastHorizontal){
+            wait();
+        }
+        checkNewLine();
+        System.out.print("-");
+        symbolsInLine++;
+        lastHorizontal = true;
+        notify();
+    }
+    public synchronized void printVertical() throws InterruptedException{
+        while(!lastHorizontal){
+            wait();
+        }
+        checkNewLine();
+        System.out.print("|");
+        symbolsInLine++;
+        lastHorizontal = false;
+        notify();
+    }
+}
